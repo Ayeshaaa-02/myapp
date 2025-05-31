06ccb16c011aa4ab8928e87864dba46b2b0a7613
Ayeshaaa-02 <ayeshaa.22.becs@acharya.ac.in>
2025-05-31 04:19:35 +0000

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class App 
+{
+	public int add(int a ,int b)
+	{
+		return a+b;
+	}
+    public static void main( String[] args )
+    {
+	App app = new App();
+	int result = app.add(3,5);
+        System.out.println( "3 + 5 = " + result );
+    }
+}
